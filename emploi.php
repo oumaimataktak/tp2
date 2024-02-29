68964f38c7651a5d3f574af6a833ee396d60c658
oumaimataktak <161769592+oumaimataktak@users.noreply.github.com>
2024-02-29 23:13:05 +0200

    Update emploi.php

--- emploi.php
+++ emploi.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 
+
+
+
+
 $jours = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi'];
 $heures = ['08-09', '09-10', '10-11', '11-12', '12-13', '13-14', '14-15', '15-16', '16-17'];
 
